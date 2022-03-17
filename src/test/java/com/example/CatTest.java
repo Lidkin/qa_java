b3ee9602348d9abfc209ab852fba0391a28eaecb
Lidkin <lidkin79@gmail.com>
2022-03-17 20:19:42 +0200

    added tests for class cat

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,15 +2,29 @@ package com.example;
 
 import org.junit.Test;
 
+
+import java.util.List;
+
 import static org.junit.Assert.*;
+
 
 public class CatTest {
 
+
     @Test
-    public void getSound() {
+    public void catSoundShouldBe() {
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        assertEquals("Это точно кошка?", "Мяу", cat.getSound());
     }
 
     @Test
-    public void getFood() {
+    public void catFoodShouldBe() throws Exception {
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        assertEquals("Это кошачья еда?", List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+
     }
+
+
 }
