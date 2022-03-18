5c42aa56313d2f86fae282920b32d73e06a260f1
Lidkin <lidkin79@gmail.com>
2022-03-18 23:18:58 +0200

    Added tests for Lion whit parameters

---
+++ src/test/java/com/example/LionWithParametersOfGenderTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+
+public class LionWithParametersOfGenderTest {
+    private final String lionSex;
+
+
+    public LionWithParametersOfGenderTest(String lionSex) {
+        this.lionSex = lionSex;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getLionGender() {
+        return new Object[][]{
+                {"Самка"},
+                {"Самец"},
+                {"не определено"},
+        };
+    }
+
+
+    @Test
+    public void doesHaveMane() throws Exception {
+        Lion lion = new Lion(lionSex, new Feline());
+        assertEquals(true, lion.doesHaveMane());
+
+    }
+
+    @Test
+    public void lionFood() throws Exception {
+        Lion lion = new Lion(lionSex, new Feline());
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+    }
+
+}
