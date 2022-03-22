f62c563b28599d7a9fd78427d2aa519083a8b289
Lidkin <lidkin79@gmail.com>
2022-03-22 19:01:56 +0200

    renamed from CatFoodAndOtherSoundMockTest, deleted sound and kittens tests

---
+++ src/test/java/com/example/CatFoodMockTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CatFoodMockTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void catFoodMock() throws Exception {
+        Cat catMock = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), catMock.getFood());
+
+    }
+
+}
